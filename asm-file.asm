8f379841b204e31b9f076a729c2979973574394a
HussainNaweed <hassannaweed264@gmail.com>
2024-12-30 12:49:08 +0500

    commit1

--- asm-file.asm
+++ asm-file.asm
@@ -1,33 +1,28 @@
-
-
 include irvine32.inc
 
-
 .data
-;public asmfunc
-msg db "assebly procedure end now",0
-
+msg db "assembly procedure end now", 0
 
 .code
-asmfunc PROC p1:DWORD, p2:DWORD
+asmfunc PROC p1: DWORD, p2: DWORD
     
-   mov esi,p1
-   mov edi,p2
+    mov esi, p1
+    mov edi, p2
+    xor ecx, ecx
+    mov ecx, 10  
 
-   mov ecx,10
-
- pass:
-    mov eax,[esi]
-    test eax, 8000h
+pass:
+    mov eax, [esi]
+    test eax, 80000000h 
     jnz skip
-    mov eax,[edi]
-    add eax,1
-    mov [edi],eax
-    
+    mov eax, [edi]
+    add eax, 1
+    mov [edi], eax
 
 skip:
-    add esi,4
+    add esi, 4          
     loop pass
     ret 
-asmfunc ENDP
-end
+
+asmfunc ENDP
+END
--- c-file.c
+++ c-file.c
@@ -1,34 +1,34 @@
 #include <stdio.h>
-
-
-//extern "C" void __stdcall asmfunc(void);
+#include <conio.h> // For getch()
 
 #ifdef __cplusplus
 extern "C" {
 #endif
 
-void __stdcall asmfunc(int arr ,int * p2);
+void __stdcall asmfunc(int* arr, int* num);
 
 #ifdef __cplusplus
 }
 #endif
 
-
 int main() {
     system("cls");
-    int arr[10]={12,1,34,45,-56,9,-4,-3,-34,-13};
-    int num=0;
-    printf("assembly proc calling from  from C! \n");
-getch();
 
-    for(int i=0;i<10;i++){
-        printf("%d",arr[i]);
-        printf(" ");
+    int arr[10] = {12, 1, 34, 45, -56, 9, -4, -3, -34, -13};
+    int num = 0;
+
+    printf("Assembly procedure called from C!\n");
+    getch();
+
+    printf("Array elements: ");
+    for (int i = 0; i < 10; i++) {
+        printf("%d ", arr[i]);
     }
-    asmfunc(arr,&num); //assembly proc calling
-   
-   getch();
-    
-    
-    printf("the total number of posiive values is :  %d \n ", num); // printing in c
+    printf("\n");
+
+    asmfunc(arr, &num); // Call the assembly procedure
+
+    getch();
+
+    printf("The total number of positive values is: %d\n", num);
 }
