fd1c2150767b844e45414d8b10ee6728acbb006e
HussainNaweed <hassannaweed264@gmail.com>
2024-12-30 12:27:46 +0500

    Second last commit (23-Ntu-cs--165)

--- asm-file.asm
+++ asm-file.asm
@@ -1,6 +1,8 @@
 
 
-INCLUDE Irvine32.inc
+include irvine32.inc
+
+
 .data
 ;public asmfunc
 msg db "assebly procedure end now",0
@@ -9,19 +11,23 @@ msg db "assebly procedure end now",0
 .code
 asmfunc PROC p1:DWORD, p2:DWORD
     
-    push ebp
-    mov ebp,esp
-    mov eax, [ebp+12
-\] ; Move the first parameter into EAX
-    call writedec
-    call crlf
-    ;add eax, p2 ; Add the second parameter to EAX
-    call writedec
-    call crlf
-    mov edx, offset msg
-    call writestring
-    call crlf
+   mov esi,p1
+   mov edi,p2
+
+   mov ecx,10
+
+ pass:
+    mov eax,[esi]
+    test eax, 8000h
+    jnz skip
+    mov eax,[edi]
+    add eax,1
+    mov [edi],eax
     
-    ret
-asmfunc ENDP
+
+skip:
+    add esi,4
+    loop pass
+    ret 
+asmfunc ENDP
 end
